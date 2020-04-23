184ae640022bad0d492746e412ff7a9c2e1f2b14
David Horstman <davidzilla27@gmail.com>
2020-04-23 15:56:14 -0400

    Move API call

---
+++ src/api/fetchShow.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+export default function fetchShow() {
+    return axios
+        .get(
+            "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        );
+}
